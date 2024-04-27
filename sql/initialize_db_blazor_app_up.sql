1f13ec445e570b156806403b9c247908c9dbb8fe
synthetic-w <teddybeargh@gmail.com>
2024-04-27 17:31:06 +0300

    SQL initialize table script fix

--- sql/initialize_db_blazor_app_up.sql
+++ sql/initialize_db_blazor_app_up.sql
@@ -3,17 +3,17 @@ USE blazor_app_db;
 CREATE TABLE userdata
 (
     Id int IDENTITY (1 , 1) PRIMARY KEY NOT NULL,
-	RegistrationDate datetime2(0),
-    Username nvarchar(40),
+	Username nvarchar(40),
+    RegistrationDate datetime2(0),
     UserRating int,
     PhoneNumber nvarchar(11),
     Email nvarchar(30)
 )
 
-INSERT INTO userdata (RegistrationDate, Username, UserRating, PhoneNumber, Email)
+INSERT INTO userdata (Username, RegistrationDate, UserRating, PhoneNumber, Email)
 VALUES
-    ('11/15/2022', 'Ivanov Alexander', 0, '79012345678', 'a.ivanov@ivanovmail.com'),
-	('01/10/2021', 'Petrov Ivan', 11, '79868891257', 'petrov.i@petrushka.com'),
-	('06/12/2023', 'Egor Blanskiy', 100, '79752115640', 'e.blanskiy@ghbdtn.ru'),
-	('04/20/2024', 'Pupa Sergey', 69, '79957305559', 'pupa@buhgalterii.net'),
-	('04/21/2024', 'Lupa Zahar', -999, '79942281489', 'lupa@buhgalterii.net')
+    ('Ivanov Alexander', '11/15/2022', 0, '79012345678', 'a.ivanov@ivanovmail.com'),
+	('Petrov Ivan', '01/10/2021', 11, '79868891257', 'petrov.i@petrushka.com'),
+	('Egor Blanskiy', '06/12/2023', 100, '79752115640', 'e.blanskiy@ghbdtn.ru'),
+	('Pupa Sergey', '04/20/2024', 69, '79957305559', 'pupa@buhgalterii.net'),
+	('Lupa Zahar', '04/21/2024', -999, '79942281489', 'lupa@buhgalterii.net')
