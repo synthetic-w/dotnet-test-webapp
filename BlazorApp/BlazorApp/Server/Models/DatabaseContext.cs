d7e717fbcd1ab7cd000c38e9561dd4e367664ec2
synthetic-w <teddybeargh@gmail.com>
2024-04-27 19:18:40 +0300

    Add data access layer

---
+++ BlazorApp/BlazorApp/Server/Models/DatabaseContext.cs
@@ -0,0 +1,42 @@
+﻿using BlazorApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BlazorApp.Server.Models
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("userdata");
+                entity.Property(e => e.UserId).HasColumnName("UserId");
+                entity.Property(e => e.Username)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+                entity.Property(e => e.RegistrationDate);
+                entity.Property(e => e.UserRating);
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+                entity.Property(e => e.EmailId)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ BlazorApp/BlazorApp/Server/Services/UserManager.cs
@@ -0,0 +1,100 @@
+﻿using BlazorApp.Server.Interfaces;
+using BlazorApp.Server.Models;
+using BlazorApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BlazorApp.Server.Services
+{
+    public class UserManager : IUser
+    {
+        readonly DatabaseContext _dbContext = new();
+
+        public UserManager(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //To get all data of all users
+        public List<User> GetUserDetails()
+        {
+            try
+            {
+                return _dbContext.Users.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To add new user data
+        public void AddUser(User user)
+        {
+            try
+            {
+                _dbContext.Users.Add(user);
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To update existing user data
+        public void UpdateUserDetails(User user)
+        {
+            try
+            {
+                _dbContext.Entry(user).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To get data of a single user
+        public User GetUserData(int id)
+        {
+            try
+            {
+                User? user = _dbContext.Users.Find(id);
+                if (user != null)
+                {
+                    return user;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To delete single user
+        public void DeleteUser(int id)
+        {
+            try
+            {
+                User? user = _dbContext.Users.Find(id);
+                if (user != null)
+                {
+                    _dbContext.Users.Remove(user);
+                    _dbContext.SaveChanges();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
