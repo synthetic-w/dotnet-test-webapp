916cc3ced3b2341c71a61f6f61cfa0ba22fac0bf
synthetic-w <teddybeargh@gmail.com>
2024-04-27 17:31:30 +0300

    Add User model

---
+++ BlazorApp/BlazorApp/Shared/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public DateTime RegistrationDate { get; set; }
+        public int UserRating { get; set; } = 0;
+        public string PhoneNumber { get; set; } = null!;
+        public string EmailId { get; set; } = null!;
+    }
+}
