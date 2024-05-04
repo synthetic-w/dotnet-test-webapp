f18121e5d77f0bacaa68bd7d0436b6d73683c61b
synthetic-w <teddybeargh@gmail.com>
2024-05-04 17:30:10 +0300

    User model change

--- BlazorApp/BlazorApp/Shared/Models/User.cs
+++ BlazorApp/BlazorApp/Shared/Models/User.cs
@@ -8,11 +8,9 @@ namespace BlazorApp.Shared.Models
 {
     public class User
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string Username { get; set; } = null!;
-        public DateTime RegistrationDate { get; set; }
-        public int UserRating { get; set; } = 0;
         public string PhoneNumber { get; set; } = null!;
-        public string EmailId { get; set; } = null!;
+        public string Email { get; set; } = null!;
     }
 }
--- sql/initialize_db_blazor_app_up.sql
+++ sql/initialize_db_blazor_app_up.sql
@@ -4,16 +4,14 @@ CREATE TABLE userdata
 (
     Id int IDENTITY (1 , 1) PRIMARY KEY NOT NULL,
 	Username nvarchar(40),
-    RegistrationDate datetime2(0),
-    UserRating int,
     PhoneNumber nvarchar(11),
     Email nvarchar(30)
 )
 
-INSERT INTO userdata (Username, RegistrationDate, UserRating, PhoneNumber, Email)
+INSERT INTO userdata (Username, PhoneNumber, Email)
 VALUES
-    ('Ivanov Alexander', '11/15/2022', 0, '79012345678', 'a.ivanov@ivanovmail.com'),
-	('Petrov Ivan', '01/10/2021', 11, '79868891257', 'petrov.i@petrushka.com'),
-	('Egor Blanskiy', '06/12/2023', 100, '79752115640', 'e.blanskiy@ghbdtn.ru'),
-	('Pupa Sergey', '04/20/2024', 69, '79957305559', 'pupa@buhgalterii.net'),
-	('Lupa Zahar', '04/21/2024', -999, '79942281489', 'lupa@buhgalterii.net')
+    ('Ivanov Alexander', '79012345678', 'a.ivanov@ivanovmail.com'),
+	('Petrov Ivan', '79868891257', 'petrov.i@petrushka.com'),
+	('Egor Blanskiy', '79752115640', 'e.blanskiy@ghbdtn.ru'),
+	('Pupa Sergey', '79957305559', 'pupa@buhgalterii.net'),
+	('Lupa Zahar', '79942281489', 'lupa@buhgalterii.net')
